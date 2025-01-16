22014fba44d6d44416051c3a8f0cbdc51da8523a
taylor-tomblin <tjt0731@live.com>
2025-01-16 04:12:03 -0500

    Changed the comment in Project 1 and added some code

--- Project_1.py
+++ Project_1.py
@@ -1,25 +1,48 @@
-"""
-Author: Taylor Tomblin & Michael Luong
-Login: ttomblin, michalu
-Date: 1/9/2025
-Description: This program solves the 8-puzzle problem using the A* search algorithm with the Manhattan distance heuristic.
-The program reads the initial and goal states of the puzzle from a file and outputs the solution to the console.
-The program also outputs the number of nodes expanded and the number of nodes in the open list at the end of the search.
-"""
+# Author: Taylor Tomblin & Michael Luong
+# Login: ttomblin, michalu
+# Date: 1/9/2025
+# Description: This program solves the 8-puzzle problem using the A* search algorithm with the Manhattan distance heuristic.
+# The program reads the initial and goal states of the puzzle from a file and outputs the solution to the console.
+# The program also outputs the number of nodes expanded and the number of nodes in the open list at the end of the search.
 
-import sys
 import heapq
+
+class PuzzleState:
+  def __init__(self, state, parent=None, move=None):
+    self.state = state
+    self.parent = parent
+    self.move = move
+    self.g = 0
+    self.h = 0
+    self.f = 0
+    
+  def __lt__(self, other):
+    return self.f < other.f
+    
+  def __eq__(self, other):
+    return self.state == other.state
+    
+  def __hash__(self):
+    return hash(str(self.state))
+    
+  def __str__(self):
+    return str(self.state)
+    
+  def __repr__(self):
+    return str(self.state)
 
 def is_goal_state(state):
   # Check if current state matches
-  ...
-  return state == goal_state
+  goal_state = [(7, 8, 1),
+                (6, -, 2),
+                (5, 4, 3)]
+  return state.board == goal_state
   
 def generate_next_states(current_state, wind_direction):
   # Generate possible next states
   ...
   
-def solve_windy_puzzle(initial_state, wind_direction):
+def solve_puzzle(initial_state, wind_direction):
   visited = set()
   queue = [initial_state]
   
@@ -36,14 +59,17 @@ def solve_windy_puzzle(initial_state, wind_direction):
         queue.append(next_state)
         
   return None
+
+def reconstruct_path(state):
+    path = []
+    while state:
+        path.append(state.board)
+        state = state.parent
+    return path[::-1]
   
 initial_state = [(1, 6, 2),
                  (5, 7, 8),
                  (-, 4, 3)]
-                 
-goal_state = [(7, 8, 1),
-              (6, -, 2),
-              (5, 4, 3)]
 
 def heuristic ():
   # Determine the heuristic value
